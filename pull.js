7eee7d69c6ca696c5b4427f3be6a4b5a435eb259
EPIC <epicentre@unsw.edu.au>
2022-01-01 14:38:46 +1100

    updated data pull

--- pull.js
+++ pull.js
@@ -1,6 +1,19 @@
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 var request = require('request');
 var fs = require('fs');
+var path = require('path');
+const csv = require('fast-csv');
+
+var bus_routes = [];
+    
+fs.createReadStream(path.resolve(__dirname, 'routes.txt'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => { 
+        if (row.route_type === '3') 
+            bus_routes.push( row.route_id);
+        } )
+    .on('end', rowCount => { console.log(bus_routes); console.log(`Parsed ${rowCount} rows`); });
 
 var requestSettings = {
   method: 'GET',
@@ -11,10 +24,18 @@ var requestSettings = {
 function saveFeed() {
     request(requestSettings, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-        
-        var json = JSON.stringify(feed);
+    /*    var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
 
+        current_buses = [];
+        feed.entity.forEach(entity => {
+            if (bus_routes.includes(entity.vehicle.trip.routeId))
+            {
+              current_buses.push(entity);
+            }
+          });
+
+        var json = JSON.stringify(current_buses);//feed);
+*/
         let date_ob = new Date();
 
         // current date
@@ -25,17 +46,37 @@ function saveFeed() {
         // current year
         let year = date_ob.getFullYear();
         // current hours
-        let hours = date_ob.getHours();
+        let hours = ("0" + date_ob.getHours()).slice(-2);
         // current minutes
-        let minutes = date_ob.getMinutes();
+        let minutes = ("0" + date_ob.getMinutes()).slice(-2);
         // current seconds
-        let seconds = date_ob.getSeconds();
+        let seconds = ("0" + date_ob.getSeconds()).slice(-2);
 
-        var filename = "D:/translink/" + year + "-" + month + "-" + date + "-" + hours + "-" + minutes + "-" + seconds + ".json";
+        var folder = "D:/translink/" + year + month + date + "/" + hours + "/";
 
-        fs.writeFile(filename.toString(), json, 'utf8', () => {console.log('Written: ' + filename)});
+        if (!fs.existsSync(folder))
+          fs.mkdirSync(folder, { recursive: true });
+
+        var filename = folder + minutes + seconds + ".pb";
+        fs.writeFile(filename.toString(), body, 'utf8', () => {console.log('Written: ' + filename)});
     }
     });
 }
 
-setInterval( saveFeed, 10000 );
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function run()
+{
+ // await sleep(60000 * 60 * 4);
+  
+  saveFeed();
+
+  setInterval( saveFeed, 10000 );
+}
+
+run();
